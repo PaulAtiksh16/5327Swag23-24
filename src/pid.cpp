7b821c3d56dd714cac709387d7eebf50415998f8
Easton Nguyen <eastonnguyen@MacBook-Pro-4.attlocal.net>
2023-08-04 18:45:21 -0700

    Added Basic PID

--- src/pid.cpp
+++ src/pid.cpp
@@ -14,13 +14,69 @@ extern pros::Motor top_right_mtr;
 const double wheel_radius = 1.625;
 const double PI = 3.14159265358979323846;
 
+class PID_controller
+{
+private:
+    // Weights for PID
+    double kP;
+    double kI;
+    double kD;
+    // Milliseconds, is the time between the move function is run.
+    int time;
+
+    double error = 0;
+    double prevError = 0;
+    double integral = 0;
+    double derivative = 0;
+
+    bool atPosition = false;
+public:
+    // Constructor, create the weights 
+    PID_controller(double P, double I, double D, int T)
+    : kP{ P }, kI{ I }, kD{ D }, time{ T }
+    {        
+    }
+
+    // Update the weights
+    void updateProportion(double update) { kP = update; }
+    void updateIntegral(double update) { kI = update; }
+    void updateDerivative(double update) { kD = update; }
+
+    void updateAtPosition(bool update) { atPosition = update; }
+
+    // Measurements in inches, distance relative to starting position
+    // Returns voltage, continuously changes voltage
+    double moveTo(double targetDistance, double currentDistance)
+    {
+        error = targetDistance - currentDistance;
+
+        // Integral is weighted by the amount of iterations, weighted by time 
+        integral += error * time;
+
+        // Within 0.5 inches of desired position, reset integral to prevent future
+        // movement
+        if (fabs(error) <= 0.5)
+        {
+            integral = 0;
+            updateAtPosition(true);
+        }
+
+        // Takes how much error has changed since last iteration, and divide by 
+        // time to determine how fast or slow it moves between iterations.
+        derivative = (error - prevError) / time;
+        prevError = error;
+
+        // Proportion part is propWeight times error (gets smaller as 
+        // bot gets closer to target distance)
+        double output = kP * error + kI * integral + kD * derivative;
+        return output;
+    }
+};
+
 double get_distance_in(double encoder_value)
 {
     // Value in centidegrees, 100 centidegrees in 1 degree
     return ((encoder_value / 100) / 360) * (2 * PI * wheel_radius);
 }
 
-void pid(double distance_moved, double target_distance)
-{
-    
-}
+
